48fd2fb191e2987d1fd2d2c91dd697b9988245d0
eamarelo <eamarelo@hotmail.fr>
2018-06-08 12:30:41 +0200

    add update method

--- app/controllers/user/update.js
+++ app/controllers/user/update.js
@@ -1,10 +1,6 @@
 // Core
 const mock = require('../../models/get-user.js')
-const validator = require('node-validator')
 const userSchema = require('../../models/userSchema.js');
-
-const check = validator.isObject()
-.withRequired('name', validator.isString())
 
 module.exports = class Update {
   constructor (app) {
@@ -17,27 +13,21 @@ module.exports = class Update {
    * Middleware
    */
    middleware () {
-    this.app.put('/user/update/:_id', validator.express(check), (req, res) => {
-      console.log("here")
+    this.app.put('/user/update/:id', (req, res) => {
       try {
-        var name = req.body.name;
-        var age = req.body.age;
-        var gender = req.body.gender;
-
-        userSchema.findById(req.params._id, function(err, userSchema) {
-          if (err){
-            res.send(err);
-          }
-                    // Mise à jour des données de la piscine
-                    userSchema.name = req.body.name;
-                    userSchema.age = req.body.age;
-                    userSchema.gender = req.body.gender;
-                    userSchema.save(function(err){
-                      res.json({message : 'Bravo, mise à jour des données OK'});
-                    });                
-                  });
-        userSchema.update({ name: name, age: age, gender: gender});
-        res.status(200).json(userSchema || {})
+        userSchema();
+        if (!req.params || !req.params.id.length) {
+          res.status(404).json({
+            code: 404,
+            message: 'Not Found'
+          })
+        }
+        userSchema.findOne({_id: req.params.id}, function (err, user) { 
+          console.log(err)
+        });
+        userSchema.update({ name: req.body.name, age: req.body.age, gender: req.body.gender}, function (err, user){
+          res.status(200).json(user || {})
+        });
       } catch (e) {
         console.log(e)
         console.error(`[ERROR] user/update/:id -> ${e}`)
